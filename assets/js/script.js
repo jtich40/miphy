688bcf16e664bf015f749f7497fdfade68194716
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-24 18:48:19 -0600

    starting js for data fetch

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let searchBtn = document.getElementById('search')
+let searchParams = document.getElementById('text-input')
+let apiKey = '1851b49e'
+let apiKey2 = 'Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99'
+
+function findMovie () {
+    let movieUrl = `http://www.omdbapi.com/?&apikey=${apiKey}&s=${searchParams}`
+    var giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99&q=Movies&limit=25&offset=0&rating=g&lang=en`
+
+
+}
+
+
