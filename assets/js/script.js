b62c83a5812175a3cb0572e99eb7ce3fe9235cce
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-24 19:17:06 -0600

    started js by adding DOM variables and starting search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-let searchBtn = document.getElementById('search')
+let searchBtn = document.getElementById('search-btn')
 let searchParams = document.getElementById('text-input')
 let apiKey = '1851b49e'
 let apiKey2 = 'Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99'
 
 function findMovie () {
     let movieUrl = `http://www.omdbapi.com/?&apikey=${apiKey}&s=${searchParams}`
-    var giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99&q=Movies&limit=25&offset=0&rating=g&lang=en`
-
-
+    var giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey2}Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99&q=${searchParams}&limit=25&offset=0&rating=g&lang=en`
 }
 
 
