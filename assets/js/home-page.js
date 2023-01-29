ecb14c8a1691366bb87f3a052b1d3facabe92e62
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-28 21:34:28 -0600

    added js file for movie quote array and added logic for quotes generating on home page

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,8 +1,6 @@
+// global DOM variables
 let searchBtn = document.getElementById('search-btn')
 let searchParams = document.getElementById('text-input')
-let movieResultsContainer = document.getElementById('search-results')
-let apiKey = '1851b49e'
-let apiKey2 = 'Nq178l1PC1zOvSDhLsOWhDI0FwqXPI99'
 
 searchBtn.addEventListener('click', function () {
     // text input entered by user
@@ -17,9 +15,42 @@ searchBtn.addEventListener('click', function () {
     window.location.href = './search-results.html'
 })
 
+function generateQuote () {
+  // container for randomly generated movie quote
+  let quoteContainer = document.getElementById('quotes')
+  // pick random quote/character/movie data
+  let randomQuote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)]
+  // console.log(randomQuote)
+ 
+  // generate quote for container
+  let quoteEl = document.createElement('span')
+  quoteEl.setAttribute('class', 'quote')
+  quoteEl.innerHTML = `"${randomQuote.quote}"`
+  quoteContainer.appendChild(quoteEl)
+  // console.log(quoteEl)
+
+  // generate character for container
+  let characterEl = document.createElement('span')
+  characterEl.setAttribute('class', 'character')
+  characterEl.innerHTML = ` - ${randomQuote.character}, `
+  quoteContainer.appendChild(characterEl)
+  // console.log(characterEl)
+
+  // generate movie for container
+  let movieEl = document.createElement('span')
+  movieEl.setAttribute('class', 'movie')
+  movieEl.innerHTML = `${randomQuote.movie}`
+  quoteContainer.appendChild(movieEl)
+  // console.log(movieEl)
+}
+
+window.onload = function () {
+  generateQuote()
+}
 
 
-//CAROUSEL
+
+// CAROUSEL
 
 const $ = selector => {
     return document.querySelector(selector);
