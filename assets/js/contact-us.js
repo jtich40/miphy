f1a00b65bf0bd3bf067ad2d12e5cba26f9ddcbe1
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-31 21:49:13 -0600

    added timer for feedback to removed, cleaned up bulma styling on form, added dummy email for feedback submissions

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,8 +1,15 @@
+// global DOM variables
 let submitBtn = document.getElementById('submit')
 let feedback = document.getElementById('feedback')
 
+// event listener to provide confirmation message when user submits feedback
 submitBtn.addEventListener('click', function() {
-    let confirmText = document.createElement('p')
+    let confirmText = document.createElement('p')    
     confirmText.innerHTML = 'Thank you for the feedback!'
     feedback.appendChild(confirmText)
+    
+    // confirmation message is removed after 2 seconds
+    setTimeout(function() {
+        confirmText.setAttribute('class', 'hide')
+    },2000)
 })
